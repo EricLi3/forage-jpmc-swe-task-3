1ba712652487dc8d561484b64d85b1767d9e829b
EricLi3 <ericlibeibei@gmail.com>
2024-01-05 13:38:35 -0500

    Task 3 of Forage JPmorgan

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,32 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number, 
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upperbound: number, 
+  lowerbound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+    const price_ABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) /2; // calculating price of the stocks using Task 1.
+    const price_DEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) /2; // using the array from serverResponse where 1st is ABC 2nd is DEF
+    const ratio = price_ABC / price_DEF;
+    const upperBound = 1.05;
+    const lowerBound = 0.95;
+    return {
+      price_abc: price_ABC,  
+      price_def: price_DEF,
+      ratio,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? 
+        serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upperbound: upperBound,
+      lowerbound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
   }
 }
